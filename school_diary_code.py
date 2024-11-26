baab48de7a40f2ce212893c0a8777494a41bf3b6
Tea With Urgant <TeaWithUrgant@gmail.com>
2024-11-26 23:21:48 +0300

    add first file

--- school_diary_code.py
+++ school_diary_code.py
@@ -1,5 +1,18 @@
-a=''
-for i in range(5):
-    b=input()
-    a+=b
-    print(a)
+class visiting:
+    def __init__(self,name,date_of_birth,grade):
+        self.name=name
+        self.date_of_birth=date_of_birth
+        self.grade=grade
+
+
+        pass
+
+class rates:
+    def __init__(self,name,subject):
+        self.name=name
+        self.subject=subject
+
+class students:
+    def __init__(self,name):
+        self.name=name
+    pass
